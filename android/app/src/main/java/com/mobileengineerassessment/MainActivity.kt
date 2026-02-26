8fce6e5706339bceebd4b466eb98591125641714
Tharmes <tharmeselvam@gmail.com>
2026-02-26 12:35:02 +0800

    Initial commit
    Generated by react-native@0.84.0

---
+++ android/app/src/main/java/com/mobileengineerassessment/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.mobileengineerassessment
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "MobileEngineerAssessment"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
